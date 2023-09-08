c847196608486d1402675e1c89797e887f842632
zzb <619082651@qq.com>
2023-09-08 20:33:02 +0800

    创建vue项目文件，用于重构前端

---
+++ vue_temp/vue/crawl_system_frontend/src/components/HeaderBox.vue
@@ -0,0 +1,72 @@
+<template>
+    <!-- 固定的顶部导航栏 -->
+    <div class="top-nav" height="auto">
+      <div class="nav-content">
+        <!-- 左侧标题 -->
+        <h1 class="nav-title">爬虫管理系统</h1>
+        <!-- 右侧按钮 -->
+        <el-button id="sync_btn" type="primary" @click="startCrawling">开始爬取</el-button>
+      </div>
+    </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    startCrawling() {
+      // 处理开始爬取的逻辑
+    }
+  }
+};
+</script>
+
+<style scoped>
+/* 顶部导航条 */
+.top-nav {
+  color: #409EFF;
+  border-bottom: 1px solid #ccc;
+  margin: 0 auto;
+  /* position: fixed;
+  top: 10px;
+  left: 0;
+  right: 0;
+  z-index: 100;
+   */
+}
+
+.nav-content {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+/* 标题 */
+.nav-title {
+  font-size: 45px;
+  font-weight: bold;
+  border-bottom: 3px solid #409EFF;
+  margin: 0;
+  padding: 20px;
+  padding-bottom: 30px;  /* 抵消按钮的margin-bottom */
+}
+
+.nav-title:hover {
+    background: #d9ecff;
+    cursor: pointer;
+}
+
+
+/* 爬取按钮 */
+#sync_btn {
+  font-size: 35px;
+  padding: 33px;
+  background: #409eff;
+  color: #fff;
+  border-radius: 15px;
+  margin-bottom: 10px;
+}
+
+#sync_btn:hover {
+  background: #337ecc;
+}
+
+</style>
---
+++ vue_temp/vue/crawl_system_frontend/src/main.js
@@ -0,0 +1,14 @@
+import Vue from 'vue'
+import App from './App.vue'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import VXETable from 'vxe-table'
+import 'vxe-table/lib/style.css'
+
+Vue.config.productionTip = false
+
+Vue.use(VXETable)
+Vue.use(ElementUI);
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
